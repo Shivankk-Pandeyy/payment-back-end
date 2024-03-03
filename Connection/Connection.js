277aa99f76cb5946f25309559b20093ff8978207
Shivank Pandey <125184309+Shivankk-Pandeyy@users.noreply.github.com>
2024-03-03 13:34:53 +0530

    Update Connection.js

--- Connection/Connection.js
+++ Connection/Connection.js
@@ -1,7 +1,9 @@
 const mongoose=require('mongoose');
+const dotenv=require('dotenv').config()
+const mongoPassword=process.env.MONGO_PASSWORD;
 const ConnectDb=async()=>{
     try{
-        await mongoose.connect("mongodb+srv://pandeyshivank21:VUp3QyGie0LvzaGx@cluster1.slqudby.mongodb.net/rhinoreps?retryWrites=true&w=majority&appName=Cluster1");
+        await mongoose.connect(`mongodb+srv://pandeyshivank21:${mongoPassword}@cluster1.slqudby.mongodb.net/rhinoreps?retryWrites=true&w=majority&appName=Cluster1`);
         console.log("MongoDb Connected");
     }
     catch(err){
