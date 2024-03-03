ca609a7843c52c1bf0492181a7dec408a34abcf3
Shivank Pandey <125184309+Shivankk-Pandeyy@users.noreply.github.com>
2024-03-03 15:04:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ const products_routes=require('./Routes/Products');
 const admin_routes=require("./Routes/Admin_Route");
 //MIDDLEWARE
 app.use(express.json());
-app.use(cors(
-    {
-        origin:["https://payment-front-end.vercel.app"],
-        methods:["POST","GET"],
-        credentials:true,
-    }
-));
+app.use(cors());
 //FOR PDF FILES MADE STATIC
 app.use("/files",express.static("files"));
 //MIDDLEWARE FOR ROUTER
